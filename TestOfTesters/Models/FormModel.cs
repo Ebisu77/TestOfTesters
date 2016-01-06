deceb4e98b7528f34ca2bf2f2770278e6f6c701c
Ebisu77 <sefc.mario@gmail.com>
2016-01-06 10:10:06 +0100

    Initial Commit

---
+++ TestOfTesters/Models/FormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestOfTesters.Models
+{
+    public class FormModel
+    {
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+    }
+}
